a88256cdf90f08d1478d83db7e826e8c9c17cc36
nay60 <coulombeljeremy@outlook.fr>
2025-02-27 14:34:52 +0100

    Authentification Supabase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "fr.sumapp.sumapp"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
