04352a9e56bf5e028d4cbd84185e416ecf09177a
nay60 <coulombeljeremy@outlook.fr>
2025-03-10 16:48:31 +0100

    Push scrap le monde, debut actualité view

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,23 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'scrap.dart';
 import 'views/actualité_view.dart';
 import 'views/actuality_date_view.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
 
 void main()async{
   await Supabase.initialize(
     url: 'https://jqtwfuiwgxnfxdyfgyln.supabase.co',  // Remplace avec ton URL Supabase
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxdHdmdWl3Z3huZnhkeWZneWxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1NzgwMzQsImV4cCI6MjA1NjE1NDAzNH0.LMVmbj5A8A7dbRJ4xRzh5JChbEzao6Bc6WkUXJZlgco',            // Remplace avec ta clé Anon
   );
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('fr', null);
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ActualityDateView(),
+      home: HtmlFetcher(),
     );
   }
 }
