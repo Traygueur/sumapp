918c52e05689f372fc4f4714ea68f9c83fb852ec
nay60 <coulombeljeremy@outlook.fr>
2025-03-10 16:49:19 +0100

    Push scrap le monde, debut actualité view

--- lib/scrap.dart
+++ lib/scrap.dart
@@ -1,4 +1,4 @@
-import 'views/actualité_view.dart';
+import 'views/actuality_view.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
