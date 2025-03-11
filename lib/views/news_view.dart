857d7159e732e8040ccec57941f2ff93ad641061
traygueur <matteodurand22@gmail.com>
2025-03-11 21:55:23 +0100

    Application done

--- lib/views/news_view.dart
+++ lib/views/news_view.dart
@@ -3,6 +3,7 @@ import 'package:sumapp/models/request_script.dart';
 import 'package:sumapp/models/scrap.dart';
 import 'package:sumapp/views/read_article_view.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class newsScreen extends StatefulWidget {
   const newsScreen({super.key});
@@ -14,6 +15,7 @@ class newsScreen extends StatefulWidget {
 class _newsScreenState extends State<newsScreen> {
   String text = "";
   List<String> titles = globalArticleTitles.entries
+      .where((entry) => entry.value[0] == DateFormat("yyyy/MM/dd").format(DateTime.now()))
       .map((entry) => entry.key)
       .toList();
 
